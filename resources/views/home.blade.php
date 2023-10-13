22ebee5db5fb22eb67b67e1b1c2a2c63eac4db54
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-10-13 21:21:06 +0300

    add named paths

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
 </head>
+
 <body>
-<a href="/pharmacy/public/signup"><button>signup</button></a>
-<a href="/pharmacy/public/loginform"><button>login</button></a>
-<a href="/pharmacy/public/updateuserform"><button>update user's data</button></a>
-<a href="/pharmacy/public/deleteuserform"><button>delete user</button></a>
-<a href="/pharmacy/public/addproductform"><button>Add product</button></a>
+    <a href="{{ route('signupform') }}"><button>signup</button></a>
+    <a href="{{ route('loginform') }}"><button>login</button></a>
+    <a href="{{ route('updateuserform') }}"><button>update user's data</button></a>
+    <a href="{{ route('deleteuserform') }}"><button>delete user</button></a>
+    <a href="{{ route('addproductform') }}"><button>Add product</button></a>
 </body>
+
 </html>
