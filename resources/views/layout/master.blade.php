a139e5734166adbdaf9793566edec0580cde2b7b
hazem mohamed <hazemm7622@gmail.com>
2023-10-22 17:50:08 +0200

    adding layout for pages

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'unKnown page')</title>
-    
+    <link rel="stylesheet" href="public">
 </head>
 <body>
     {{-- start navbar --}}
