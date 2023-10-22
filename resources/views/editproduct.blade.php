8f322ab3e1a4cdf20bca210e73e8804479467931
Moniem <abdulrahman2111656@miuegypt.edu.eg>
2023-10-23 01:05:48 +0300

    update product page

--- resources/views/editproduct.blade.php
+++ resources/views/editproduct.blade.php
@@ -1,75 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<style>
-    body {
-    font-family: Arial, sans-serif;
-    background-color: #838392f8;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    margin: 0;
-}
+@extends('aside_bar')
 
-form {
-    background-color:  #000039;
-    border-radius: 10px;
-    box-shadow: 10px 10px 20px 7px rgb(10, 10, 10);
-    padding: 40px;
-    width: 300px;
-    text-align: center;
-}
 
-label {
-    color: #fff;
-    display: block;
-    margin-bottom: 10px;
-    text-align: left;
-    font-weight: bold;
-}
+@section('name')
+   
+   
+@endsection
+@section('card')
 
-input[type="text"], input[type="file"] {
-    width: calc(100% - 20px);
-    padding: 10px;
-    margin-bottom: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    box-sizing: border-box;
-    font-size: 16px;
-}
-
-input[type="file"] {
-    cursor: pointer;
-}
-
-img {
-    margin: 10px 0;
-    border-radius: 5px;
-}
-
-button[type="submit"] {
-    background-color: #1c1cf0;
-    border: none;
-    border-radius: 5px;
-    color: black;
-    cursor: pointer;
-    font-size: 16px;
-    padding: 10px 20px;
-    transition: background-color 0.3s ease-in-out;
-}
-
-button[type="submit"]:hover {
-    background-color: #0000cd;
-}
-
-</style>
-<body>
 
     @if (count($errors) > 0)
     <div class="card mt-5">
@@ -100,5 +37,4 @@ button[type="submit"]:hover {
 
         
     </form> 
-</body>
-</html>
+@endsection
