8988e4cbede71cd6ab22eafb599a3f67e11059fb
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-10-22 21:13:00 +0300

    link all pages togerher

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,234 +1,203 @@
 @extends('layout.master')
 
-@section('title','home')
-    
+@section('title', 'home')
+
 
 @section('content')
     <!-- start slider -->
-    <div
-      id="carouselExampleControls"
-      class="carousel slide"
-      data-bs-ride="carousel"
-    >
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="images/baby.webp" class="d-block w-100" alt="baby-Photo" />
+
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/baby.webp" class="d-block w-100" alt="baby-Photo" />
+            </div>
+            <div class="carousel-item">
+                <img src="images/back.webp" class="d-block w-100" alt="backToSchool-Photo" />
+            </div>
+            <div class="carousel-item">
+                <img src="images/cosmetics.webp" class="d-block w-100" alt="cosmetics-Photo" />
+            </div>
         </div>
-        <div class="carousel-item">
-          <img
-            src="images/back.webp"
-            class="d-block w-100"
-            alt="backToSchool-Photo"
-          />
-        </div>
-        <div class="carousel-item">
-          <img
-            src="images/cosmetics.webp"
-            class="d-block w-100"
-            alt="cosmetics-Photo"
-          />
-        </div>
-      </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 
     <!-- end of slider -->
     <div class="our-work text-center  pb-5 pt-5 ">
-      <div class="container">
-        <div class="main-title mt-5 mb-5 position-relative">
-          <img class="mb-4" src="images/saydlya.png" alt="" />
-          <h2>We have all you need</h2>
-          <p class="text-black-50 text-uppercase">high quality low price</p>
+        <div class="container">
+            <div class="main-title mt-5 mb-5 position-relative">
+                <img class="mb-4" src="images/saydlya.png" alt="" />
+                <h2>We have all you need</h2>
+                <p class="text-black-50 text-uppercase">high quality low price</p>
+            </div>
+            <div class="row">
+
+                <a href="#" class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <div class="box mb-3 bg-white" data-work="Baby care">
+                            <img class="img-fluid" src="images/BabyCare.png" alt="" />
+
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#" class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <div class="box mb-3 bg-white" data-work="Skin care">
+                            <img class="img-fluid" src="images/FACE-CARE_1.jpg" alt="" />
+                        </div>
+                    </div>
+                </a>
+
+
+                <a href="#" class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <div class="box mb-3 bg-white" data-work="Medications">
+                            <img class="img-fluid" src="images/Medications.png" alt="" />
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#" class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <div class="box mb-3 bg-white" data-work="Vitamins">
+                            <img class="img-fluid" src="images/Vitamins-and-Minerals_1.png" alt="" />
+                        </div>
+                    </div>
+                </a>
+
+
+                <a href="#" class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <div class="box mb-3 bg-white" data-work="Supplements">
+                            <img class="img-fluid" src="images/Supplements.jpg" alt="" />
+
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#" class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <div class="box mb-3 bg-white" data-work="medical equipments">
+                            <img class="img-fluid" src="images/Medicalequipments.png" alt="" />
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#" class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <div class="box mb-3 bg-white" data-work="bills">
+                            <img class="img-fluid" src="images/Bills.png" alt="" />
+                        </div>
+                    </div>
+                </a>
+
+
+
+                <a href="about-us.html" class="col-sm-6 col-md-4 col-lg-3">
+                    <div>
+                        <div class="box mb-3 bg-white" data-work="antibiotique">
+
+                            <img class="img-fluid" src="images/Anti-biotique.png" alt="" />
+
+
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="d-flex justify-content-center mt-5">
+                <a class="btn btn-primary rounded-bill main-btn" href="#">Show all products</a>
+            </div>
         </div>
-        <div class="row">
-
-          <a href="#" class="col-sm-6 col-md-4 col-lg-3">
-          <div >
-            <div class="box mb-3 bg-white" data-work="Baby care">
-              <img class="img-fluid" src="images/BabyCare.png" alt="" />
-                
-            </div>
-          </div>
-        </a>
-
-          <a href="#" class="col-sm-6 col-md-4 col-lg-3"> 
-            <div>
-            <div class="box mb-3 bg-white" data-work="Skin care">
-              <img class="img-fluid" src="images/FACE-CARE_1.jpg" alt="" />
-            </div>
-          </div>
-        </a>
-         
-
-          <a href="#" class="col-sm-6 col-md-4 col-lg-3" >
-          <div >
-            <div class="box mb-3 bg-white" data-work="Medications">
-              <img class="img-fluid" src="images/Medications.png" alt="" />
-            </div>
-          </div>
-          </a>
-
-          <a href="#" class="col-sm-6 col-md-4 col-lg-3">
-          <div >
-            <div class="box mb-3 bg-white" data-work="Vitamins">
-             <img class="img-fluid" src="images/Vitamins-and-Minerals_1.png" alt="" />
-            </div>
-          </div>
-        </a>
-
-
-          <a href="#" class="col-sm-6 col-md-4 col-lg-3">
-          <div >
-            <div class="box mb-3 bg-white" data-work="Supplements">
-              <img class="img-fluid" src="images/Supplements.jpg" alt="" />
-                
-            </div>
-          </div>
-        </a>
-
-        <a href="#" class="col-sm-6 col-md-4 col-lg-3">
-          <div >
-            <div class="box mb-3 bg-white" data-work="medical equipments">
-              <img class="img-fluid" src="images/Medicalequipments.png" alt="" />
-            </div>
-          </div>
-        </a>
-
-          <a href="#" class="col-sm-6 col-md-4 col-lg-3">
-            <div >
-              <div class="box mb-3 bg-white" data-work="bills">
-                <img class="img-fluid" src="images/Bills.png" alt="" />
-              </div>
-            </div>
-          </a>
-          
-          
-            
-         <a href="about-us.html" class="col-sm-6 col-md-4 col-lg-3">
-          <div >
-            <div class="box mb-3 bg-white" data-work="antibiotique">
-              
-                <img class="img-fluid" src="images/Anti-biotique.png" alt="" />
-                
-              
-            </div>
-          </div>
-         </a> 
-         </div> 
-        <div class="d-flex justify-content-center mt-5">
-          <a class="btn btn-primary rounded-bill main-btn" href="#">Show all products</a>
-        </div>
-      </div>
     </div>
     </div>
 
 
     <!-- discount product -->
     <section class="discount_section">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-3 col-md-5 offset-md-2">
-            <div class="detail-box">
-              <h2>
-                <strong
-                  >You get <br />
-                  any medicine <br />
-                  on
-                  <span> 10% discount </span></strong
-                >
-              </h2>
-              <p>
-                <strong
-                  >It is a long established fact that a reader will be
-                  distracted by</strong
-                >
-              </p>
-              <div>
-                <a href="#"> Buy Now </a>
-              </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-md-5 offset-md-2">
+                    <div class="detail-box">
+                        <h2>
+                            <strong>You get <br />
+                                any medicine <br />
+                                on
+                                <span> 10% discount </span></strong>
+                        </h2>
+                        <p>
+                            <strong>It is a long established fact that a reader will be
+                                distracted by</strong>
+                        </p>
+                        <div>
+                            <a href="#"> Buy Now </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-7 col-md-5">
+                    <div class="img-box">
+                        <img src="images/medicines.jpg" alt="" />
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="col-lg-7 col-md-5">
-            <div class="img-box">
-              <img src="images/medicines.jpg" alt="" />
-            </div>
-          </div>
         </div>
-      </div>
     </section>
     <!-- end of product section -->
 
     <!-- service section -->
     <div class="features text-center pt-5 pb-5">
-      <div class="container">
-        <div class="main-title mt-5 mb-5 position-relative">
-          <img class="mb-4" src="images/saydla2.png" alt="" />
-          <h2>We are Helping you 24/7</h2>
-          <p class="text-black-70 text-uppercase">
-            We provide many servies to help you
-          </p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-6 col-lg-4">
-          <div class="feat">
-            <div class="icon-holder position-relative">
-              <i class="fa-solid fa-1 position-absolute bottom-0 number"></i>
-              <i
-                class="fa-solid fa-4x fa-tablets position-absolute bottom-0 icon"
-              ></i>
+        <div class="container">
+            <div class="main-title mt-5 mb-5 position-relative">
+                <img class="mb-4" src="images/saydla2.png" alt="" />
+                <h2>We are Helping you 24/7</h2>
+                <p class="text-black-70 text-uppercase">
+                    We provide many servies to help you
+                </p>
             </div>
-            <h4 class="mt-3 mb-3 text-uppercase">High quality</h4>
-            <p class="text-black-70 lh-lg">
-              We serve you with the best quality in the market
-            </p>
-          </div>
         </div>
-        <div class="col-md-6 col-lg-4">
-          <div class="feat">
-            <div class="icon-holder position-relative">
-              <i class="fa-solid fa-2 position-absolute bottom-0 number"></i>
-              <i
-                class="fa-solid fa-4x fa-truck position-absolute bottom-0 icon"
-              ></i>
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                <div class="feat">
+                    <div class="icon-holder position-relative">
+                        <i class="fa-solid fa-1 position-absolute bottom-0 number"></i>
+                        <i class="fa-solid fa-4x fa-tablets position-absolute bottom-0 icon"></i>
+                    </div>
+                    <h4 class="mt-3 mb-3 text-uppercase">High quality</h4>
+                    <p class="text-black-70 lh-lg">
+                        We serve you with the best quality in the market
+                    </p>
+                </div>
             </div>
-            <h4 class="mt-3 mb-3 text-uppercase">Delivery 24/7</h4>
-            <p class="text-black-70 lh-lg">
-              We deliver all the day once you call us 
-            </p>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-4">
-          <div class="feat">
-            <div class="icon-holder position-relative">
-              <i class="fa-solid fa-3 position-absolute bottom-0 number"></i>
-              <i
-                class="fa-solid fa-4x fa-user-doctor position-absolute bottom-0 icon"
-              ></i>
+            <div class="col-md-6 col-lg-4">
+                <div class="feat">
+                    <div class="icon-holder position-relative">
+                        <i class="fa-solid fa-2 position-absolute bottom-0 number"></i>
+                        <i class="fa-solid fa-4x fa-truck position-absolute bottom-0 icon"></i>
+                    </div>
+                    <h4 class="mt-3 mb-3 text-uppercase">Delivery 24/7</h4>
+                    <p class="text-black-70 lh-lg">
+                        We deliver all the day once you call us
+                    </p>
+                </div>
             </div>
-            <h4 class="mt-3 mb-3 text-uppercase">Best staff</h4>
-            <p class="text-black-70 lh-lg">
-            Best doctor are available to help you 
-            </p>
-          </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="feat">
+                    <div class="icon-holder position-relative">
+                        <i class="fa-solid fa-3 position-absolute bottom-0 number"></i>
+                        <i class="fa-solid fa-4x fa-user-doctor position-absolute bottom-0 icon"></i>
+                    </div>
+                    <h4 class="mt-3 mb-3 text-uppercase">Best staff</h4>
+                    <p class="text-black-70 lh-lg">
+                        Best doctor are available to help you
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
 @endsection
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,74 +1,73 @@
 <!-- start of navbar -->
 <nav class="navbar navbar-expand-lg sticky-top">
     <div class="container">
-      <a class="navbar-brand"  href="{{ route('home') }}">
-        <img src="images/logo.png" alt="logo" />
-        
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#main"
-        aria-controls="main"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i class="fa-solid fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="main">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link p-2 p-lg-3 active" aria-current="page" href="{{route('home')}}">Home</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link p-2 p-lg-3 dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              products
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Medications</a></li>
-              <li><a class="dropdown-item" href="#">Skin Care</a></li>
-              <li><a class="dropdown-item" href="#">Baby Care</a></li>
-              <li>
-                <a class="dropdown-item" href="#">Medical equipments</a>
-              </li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Vitamins</a></li>
-              <li><a class="dropdown-item" href="#"> Supplements</a></li>
-              <li><a class="dropdown-item" href="#">Bills</a></li>
-              <li>
-                <a class="dropdown-item" href="#">antibiotique</a>
-              </li>
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img src="images/logo.png" alt="logo" />
+
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main" aria-controls="main"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="main">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-lg-3 active" aria-current="page" href="{{ route('home') }}">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link p-2 p-lg-3 dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        products
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Medications</a></li>
+                        <li><a class="dropdown-item" href="#">Skin Care</a></li>
+                        <li><a class="dropdown-item" href="#">Baby Care</a></li>
+                        <li>
+                            <a class="dropdown-item" href="#">Medical equipments</a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="#">Vitamins</a></li>
+                        <li><a class="dropdown-item" href="#"> Supplements</a></li>
+                        <li><a class="dropdown-item" href="#">Bills</a></li>
+                        <li>
+                            <a class="dropdown-item" href="#">antibiotique</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-lg-3" href="{{ route('contact') }}"> contact</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-lg-3" href="{{ route('about-us') }}"> About us</a>
+                </li>
             </ul>
-          </li>
+            <div class="icons ps-3 pe-3 d-none d-lg-block">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <i class="fa-solid fa-cart-shopping"></i>
+                <i class="fa-regular fa-heart"></i>
+            </div>
 
-          <li class="nav-item">
-            <a class="nav-link p-2 p-lg-3" href="{{route('contact')}}"> contact</a>
-          </li>
+            <div class="m-3">
+                <a class="btn btn-primary rounded-bill main-btn" href="{{ route('loginform') }}">Login</a>
+            </div>
 
-          <li class="nav-item">
-            <a class="nav-link p-2 p-lg-3" href="{{route('about-us')}}"> About us</a>
-          </li>
-        </ul>
-        <div class="icons ps-3 pe-3 d-none d-lg-block">
-          <i class="fa-solid fa-magnifying-glass"></i>
-          <i class="fa-solid fa-cart-shopping"></i>
-          <i class="fa-regular fa-heart"></i>
+            <div class="m-3">
+                <a class="btn btn-primary rounded-bill main-btn" href="{{ route('dash') }}">Dashboard</a>
+            </div>
+
+            <div class="m-3">
+                <a class="btn btn-primary rounded-bill main-btn" href="{{ route('displaycart') }}">Cart</a>
+            </div>
+
+            <div class="m-1"><a class="btn btn-primary rounded-bill main-btn"
+                    href="{{ route('signupform') }}">SignUp</a></div>
         </div>
-        <div class="m-3">
-          <a class="btn btn-primary rounded-bill main-btn" href="#">Login</a>
-        </div>
-        
-        <div class="m-1"><a class="btn btn-primary rounded-bill main-btn" href="#">SignUp</a></div>
-      </div>
     </div>
-  </nav>
+</nav>
 
-  <!-- end of navbar -->
+<!-- end of navbar -->
