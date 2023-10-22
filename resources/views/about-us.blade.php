ed217ed35ef7960119eef8691db9c83ca2c752c0
hazem mohamed <hazemm7622@gmail.com>
2023-10-22 19:48:39 +0200

    about-us page

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.master')
+
+@section('title',"About us")
+
+@section('content')
+    
+<div class="features text-center pt-5 pb-5">
+    <div class="container">
+      <div class="main-title mt-5 mb-5 position-relative">
+        <img class="mb-4" src="images/about-medicine.png" alt="" />
+        
+        <p class="text-black-70 text-uppercase">
+          We provide many services to help people get in a good health. we deal with the best companies in the market with 15 years of Experience.
+         We also offer services include dispensing prescription and over-the-counter medications, offering expert guidance on their usage, ensuring medication safety, and often providing additional healthcare products such as supplements and medical supplies. 
+        </p>
+      </div>
+    </div>
+</div>
+
+@endsection
+    
