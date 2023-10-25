838277c1b7756e91ee72c1330a9ed2841caa85f8
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-10-25 20:35:46 +0300

    group routes

--- routes/web.php
+++ routes/web.php
@@ -16,27 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
-Route::get('/dd', function () {
-    return view('dd');
-})->name('dash');
-
-Route::get('/addproduct', function () {
-    return view('add__product');
-})->name('addproduct');
-
-Route::get('/disproduct', function () {
-    return view('dis__product');
-})->name('disproduct');
-
-Route::get('/disproduct', [App\Http\Controllers\prod_cont::class, 'displayproduct'])->name('disproduct');
+Route::post('/register', [App\Http\Controllers\user_cont::class, 'store'])->name('reg');
 Route::get('/signup', function () {
     return view('signup');
 })->name('signupform');
-Route::get('/logout', [App\Http\Controllers\user_cont::class, 'logout'])->name('logout');
-
+Route::post('/isuniqueemail/{email}', [App\Http\Controllers\user_cont::class, 'isUniqueEmail'])->name("isuniqueemail");
+Route::post('/login', [App\Http\Controllers\user_cont::class, 'login'])->name('login');
 Route::get('/loginform', function () {
     return view('login');
 })->name('loginform');
+Route::get('/displayprods', [App\Http\Controllers\prod_cont::class, 'displayproductuser'])->name('displayproducts');
+Route::get('/logout', [App\Http\Controllers\user_cont::class, 'logout'])->name('logout');
+Route::get('/disproduct', [App\Http\Controllers\prod_cont::class, 'displayproduct'])->name('disproduct');
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
@@ -45,23 +36,8 @@ Route::get('/about-us', function () {
 })->name('about-us');
 
 Route::get('/allprod', [App\Http\Controllers\prod_cont::class, 'displayproductuser'])->name("produser");
-Route::post('/isuniqueemail/{email}', [App\Http\Controllers\user_cont::class, 'isUniqueEmail'])->name("isuniqueemail");
 
 Route::get('/allprod/{category}', [App\Http\Controllers\prod_cont::class, 'displayproductuserByCategory'])->name("produsercategory");
-
-
-Route::get('/dis_users', [App\Http\Controllers\user_cont::class, 'disusers'])->name('dis_users');
-
-
-
-
-
-Route::post('/register', [App\Http\Controllers\user_cont::class, 'store'])->name('reg');
-Route::post('/login', [App\Http\Controllers\user_cont::class, 'login'])->name('login');
-Route::get('/displayprods', [App\Http\Controllers\prod_cont::class, 'displayproductuser'])->name('displayproducts');
-
-
-
 
 Route::group(['middleware' => 'isloggedin'],function () {
 
@@ -74,16 +50,21 @@ Route::group(['middleware' => 'isloggedin'],function () {
     })->name('deleteuserform');
 });
 
-
 Route::group(['middleware' => 'isadmin'],function () {
-    Route::get('/addproductform', function () {
-        return view('addproduct');
-    })->name('addproductform');
+    Route::get('/addproduct', function () {
+        return view('add__product');
+    })->name('addproduct');
+    Route::get('/disproduct', function () {
+        return view('dis__product');
+    })->name('disproduct');
+    Route::get('/dd', function () {
+        return view('dd');
+    })->name('dash');
+    Route::get('/dis_users', [App\Http\Controllers\user_cont::class, 'disusers'])->name('dis_users');
     Route::get('/updateuserform/{id}', [App\Http\Controllers\user_cont::class, 'admin_show_updateuser_form'])->name('adminupdateuserform');
     Route::post('/updateuserform/{id}', [App\Http\Controllers\user_cont::class, 'admin_updateuser'])->name('adminupdateuser');
     Route::post('/updateuser', [App\Http\Controllers\user_cont::class, 'updateuser'])->name('updateuser');
     Route::post('/deleteuser', [App\Http\Controllers\user_cont::class, 'deleteuser'])->name('deleteuser');
-
     Route::post('/addprod', [App\Http\Controllers\prod_cont::class, 'addprod'])->name('addprod');
     Route::get('/displayprod', [App\Http\Controllers\prod_cont::class, 'displayproducts'])->name('displayproducts');
     Route::get('/delete/{id}', [App\Http\Controllers\prod_cont::class, 'deleteprod'])->name('deleteprod');
