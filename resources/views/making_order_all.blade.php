57da77094b921889708572a60ea4e9e4b9358fe6
hazem mohamed <hazemm7622@gmail.com>
2023-12-06 20:36:03 +0200

    update .env + online payment button

--- resources/views/making_order_all.blade.php
+++ resources/views/making_order_all.blade.php
@@ -13,21 +13,29 @@
 
     <label for="new_address">Change address:</label>
     <input type="text" id="new_address" name="new_address" >
-
     <!-- Other form fields if needed -->
-    <label>choose pay method</label>
+    <label>Choose payment method</label>
     <select name="payment" id="paymentMethod">
-        <option value="1"> cash on delivery</option>
-        <option value="2"> online payment</option>
+        <option value="1">Cash on Delivery</option>
+        <option value="2">Online Payment</option>
     </select>
-   
 
     <div class="col-2">
-        <button type="submit" class="btn btn-danger" >  Create Order
-        </button>
-            
+        <button type="submit" class="btn btn-danger">Create Order</button>
     </div>
 </form>
+
+
+<div class="container py-5 text-center">
+    <form action="{{ route('checkout') }}" method="POST">
+        @csrf
+        @method('POST')
+            <button class="btn btn-lg text-light bg-dark text-center"
+                type="submit" >create Online Order
+            </button>
+    </form>
+</div>
+
 <script>
     document.querySelector('form').addEventListener('submit', function (event) {
         // Prevent the default form submission behavior
@@ -40,7 +48,7 @@
             // Redirect to the confirm_order route
             window.location.href = "{{ route('confirm_order_all') }}";
         } else if (selectedOption === '2') {
-            // Redirect to the about-us route
+           
             window.location.href = "{{ route('checkout') }}";
         }
     });
